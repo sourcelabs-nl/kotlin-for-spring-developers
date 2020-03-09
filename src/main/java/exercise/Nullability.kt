ff43f47b2a0af58b69c186328e570f198d96b045
Stephan Oudmaijer <soudmaijer@gmail.com>
2020-03-09 08:32:15 +0100

    break the code

--- src/main/java/exercise/Nullability.kt
+++ src/main/java/exercise/Nullability.kt
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.NotNull
 class Nullability {
 
     fun fixme(@NotNull str: String?) {
-//        val length = str.length
-//        print("$str length is $length")
+        val length = str.length
+        print("$str length is $length")
     }
 }
